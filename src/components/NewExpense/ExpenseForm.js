5e8459146237eeb0aac88506ab30421190bcddf9
Pranav R <pranav81234@gmail.com>
2022-10-09 11:58:19 +0530

    added new UI components

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,106 @@
+import React, {useState} from "react";
+
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+    const [enteredTitle , setEnteredTitle] = useState('');
+    const [enteredAmount , setEnteredAmount] = useState('');
+    const [enteredDate , setEnteredDate] = useState('');
+
+    // you can pass in multiple states into the
+    // useState method as well in the given below
+    // format.
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // });
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // });
+
+        // The above method also works but
+        // This below method is more good
+        // because it guarantees that the state that is
+        // taken into updation will always be the latest
+        // state. This ensures that there is no error in 
+        // very niche cases.
+
+        // setUserInput((prevState)=>{
+        //     return {
+        //         ...prevState,
+        //         enteredTitle: event.target.value,
+        //     }
+        // })
+    };
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+        
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // });
+    };
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // });
+    };
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
+    return (
+        <form className="expense-form" onSubmit={formSubmitHandler}>
+            <div className="new-expense__controls">
+                <div className = "new-expense__control">
+                    <label>Title</label>
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
+                </div>
+                <div className = "new-expense__control">
+                    <label>Amount</label>
+                    <input type='number' value={enteredAmount} min='0.01' step="0.01" onChange={amountChangeHandler}/>
+                </div>
+                <div className = "new-expense__control">
+                    <label>Date</label>
+                    <input type='date' value={enteredDate} min="2019-01-01" max='2022-12-31' onChange={dateChangeHandler}/>
+                </div>
+            </div>
+            <div className = 'new-expense__actions'>
+                <button type = 'submit'>Add Expense</button>
+            </div>
+        </form>
+    );
+};
+
+export default ExpenseForm;
+
+// Clear forms
+// You can clear the forms on submission using the
+// concept called double or two way binding.
+// For that add the attribute value to the input fields.
+// And then set the value to the variable returned from the
+// useState method. Then call the setChange function from the 
+// useState to set the values back to the original value.
